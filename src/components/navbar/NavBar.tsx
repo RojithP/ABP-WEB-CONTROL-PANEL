de0a026a56f938cf9085aca038df4191d3c4d4cf
rojith.p <rojithp.info@gmail.com>
2024-09-21 16:07:45 +0530

    updated navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,5 +1,5 @@
 export default function NavBar() {
-    return <div className="navbar bg-base-100">
+    return <div className="navbar bg-base-100 shadow-lg">
         <div className="flex-none">
             <button className="btn btn-square btn-ghost">
                 <svg
@@ -18,20 +18,29 @@ export default function NavBar() {
         <div className="flex-1">
             <a className="btn btn-ghost text-xl">ABP PANEL</a>
         </div>
-        <div className="flex-none">
-            <button className="btn btn-square btn-ghost">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    className="inline-block h-5 w-5 stroke-current">
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
-                </svg>
-            </button>
+        <div className="form-control mr-2">
+            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+        </div>
+        <div className="dropdown dropdown-end mr-6">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                    <img
+                        alt="Tailwind CSS Navbar component"
+                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                </div>
+            </div>
+            <ul
+                tabIndex={0}
+                className="shadow-lg menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 ">
+                <li>
+                    <a className="justify-between">
+                        Profile
+                        <span className="badge">New</span>
+                    </a>
+                </li>
+                <li><a>Settings</a></li>
+                <li><a>Logout</a></li>
+            </ul>
         </div>
     </div>
 }
